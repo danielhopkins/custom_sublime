8e176e5be946681d936a0d0fbed5bb1765c0f45b
Dan Hopkins <dan.hopkins@livingsocial.com>
2012-05-29 08:37:41 -0600

    Add real pane movement

--- custom_commands.py
+++ custom_commands.py
@@ -1,4 +1,4 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, math
 
 class ToggleGutterCommand(sublime_plugin.TextCommand):
   def run(self, edit):
@@ -19,4 +19,30 @@ class ToggleDarkLightCommand(sublime_plugin.WindowCommand):
 
     for v in self.window.views():
       v.settings().set("color_scheme", new_setting)
+
+class PaneMoveCommand(sublime_plugin.WindowCommand):
+  def run(self, direction):
+    layout   = self.window.get_layout()
+    num_rows = len(layout['rows']) - 1
+    num_cols = len(layout['cols']) - 1
+    current_grp,current_idx = self.window.get_view_index(self.window.active_view())
+
+    cur_col = current_grp % num_cols
+    cur_row = (int(current_grp) / int(num_rows)) % num_rows
+
+    if direction == 'right':
+      next_col = (cur_col + 1) % num_cols
+      next_row = cur_row
+    elif direction == 'left':
+      next_col = (cur_col - 1) % num_cols
+      next_row = cur_row
+    elif direction == 'down':
+      next_col = cur_col
+      next_row = (cur_row + 1) % num_rows
+    else:
+      next_col = cur_col
+      next_row = (cur_row - 1) % num_rows
+
+    next_group = next_col + (next_row * num_rows)
+    self.window.focus_group(next_group)
 
